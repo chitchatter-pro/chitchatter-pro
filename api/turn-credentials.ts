2eba9bb62b55fded075dfe7c479f9e074817601c
Jeremy Kahn <me@jeremyckahn.com>
2025-09-16 20:07:47 -0500

    fixup! feat(closes #9): scaffold app

--- api/turn-credentials.ts
+++ api/turn-credentials.ts
@@ -2,8 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default (_req: VercelRequest, res: VercelResponse) => {
   res.status(200).send({
-    urls: 'turn:dummy-turn-server.com:3478',
-    username: 'dummy-user',
-    credential: 'dummy-password',
+    urls: process.env.TURN_URL || 'turn:dummy-turn-server.com:3478',
+    username: process.env.TURN_USERNAME || 'dummy-user',
+    credential: process.env.TURN_PASSWORD || 'dummy-password',
   })
 }
